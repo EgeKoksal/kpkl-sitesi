2f55cd5cc1ad0e6f5f904c90f1bd236913713bb4
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-05-19 15:51:59 +0300

    diğer dosyalarda eklendi ama butonda hata var

--- voek/app.js
+++ voek/app.js
@@ -1,6 +1,34 @@
 const anaDon = document.querySelector(".anasayfadön");
+const kisiAlpi = document.querySelector(".alpi");
+const kisiGoki = document.querySelector(".göki");
+const kisiLaz = document.querySelector(".laz");
+const kisiEse = document.querySelector(".ese");
+const kisiKoki = document.querySelector(".köki");
 
-anaDon.addEventListener("click", function() {
-    window.location.href = "index.html"
 
-})
+kisiAlpi.addEventListener("click", function () {
+  window.location.href = "alpi.html";
+});
+
+kisiGoki.addEventListener("click", function () {
+  window.location.href = "göki.html";
+});
+
+kisiLaz.addEventListener("click", function () {
+  window.location.href = "laz.html";
+});
+
+kisiEse.addEventListener("click", function () {
+  window.location.href = "ese.html";
+});
+
+kisiKoki.addEventListener("click", function () {
+  window.location.href = "köki.html";
+});
+
+anaDon.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+  
+
+
